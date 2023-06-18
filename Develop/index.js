7db8de0fc042d03c74e36013302916951ec31f6e
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-06-18 17:39:44 -0400

    create write to file function and create init function, create function to choose between licenses

--- Develop/index.js
+++ Develop/index.js
@@ -29,6 +29,11 @@ const questions = [
         message: 'What are your test instructions?',
         name: 'tests'
     }, {
+        type: 'list',
+        message: 'What LICENSE would you like to choose?',
+        name: 'license',
+        choices: []
+    }, {
         type: 'input',
         message: 'What is your github username?',
         name: 'github'
@@ -41,52 +46,76 @@ const questions = [
 ];
 
 function licenseBadge(license) {
-    let licenseType = license;
     let yourLicense = '';
-    if(licenseType === 'MIT') {
+    if(license === 'The MIT License') {
     yourLicense = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-    } else if (licenseType === 'Apache 2.0') {
+    } else if (license === 'Apache 2.0 License') {
     yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Boost 1.0') {
+    } else if (license === 'Boost Software License 1.0') {
     yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (licenseType === 'Apache 2.0') {
-    yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } 
+    } else if (license === 'BSD 3-Clause License') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+    } else if (license === 'BSD 2-Clause License') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)'
+    } else if (license === 'CC0') {
+        yourLicense = '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)'
+    } else if (license === 'Attribution 4.0 International') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    } else if (license === 'Attribution-ShareAlike 4.0 International') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    } else if (license === 'Attribution-NonCommercial 4.0 International') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    } else if (license === 'Attribution-NoDerivates 4.0 International') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    } else if (license === 'Attribution-NonCommmercial-ShareAlike 4.0 International') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    } else if (license === 'Attribution-NonCommercial-NoDerivatives 4.0 International') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    } else if (license === 'Eclipse Public License 1.0') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    }  else if (license === 'GNU GPL v3') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'GNU GPL v2') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    } else if (license === 'GNU AGPL v3') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'GNU LGPL v3') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'GNU FDL v1.3') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'The Hippocratic License 2.1') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'The Hippocratic License 3.0') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'IBM Public License Version 1.0') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'ISC License (ISC)') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'Mozilla Public License 2.0') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'Attribution License (BY)') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'Open Database License (ODbL)') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'Public Domain Dedication and License (PDDL)') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'The Perl License') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'SIL Open Font License 1.1') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'The Unlicense') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'The Do What the Fuck You Want to Public License') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'The zlib/libpng License') {
+        yourLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    }
     return yourLicense
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-return `# ${title} 
-
+function writeToFile(fileName, {title, license, description, installation, usage, contribution, tests, github, email}) {
+const content = `# ${title} 
 ${licenseBadge(license)}
 
 ## Description
@@ -130,11 +159,22 @@ Here is my Github profile to check out my other repositories: [My Github](https:
 
 Reach me also under my email adress for additional questions: [My email](mailto:${email})`
 
+fs.writeFileSync(fileName, content);
+
 
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(response => {
+            writeToFile('README.md', response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
 
 // Function call to initialize app
 init();
